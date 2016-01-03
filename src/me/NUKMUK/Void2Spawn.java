5114c427b903593b7ad68b7055fb2188cbdd7424
NUKMUK <rasmus.hintzmadsen@gmail.com>
2016-01-03 15:07:03 +0200

    Added website link

--- src/me/NUKMUK/Void2Spawn.java
+++ src/me/NUKMUK/Void2Spawn.java
@@ -1,15 +1,13 @@
 package me.NUKMUK;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Created by rasmu on 20.12.2015.
- */
 public class Void2Spawn extends JavaPlugin implements Listener {
 
     public void onEnable(){
@@ -20,7 +18,9 @@ public class Void2Spawn extends JavaPlugin implements Listener {
     public void onMove(PlayerMoveEvent e){
         Player p = e.getPlayer();
         if(p.getLocation().getY() <= 0){
-            p.teleport(p.getWorld().getSpawnLocation().add(0.5, 0, 0.5));
+            Location l = p.getWorld().getSpawnLocation().add(0.5, 0, 0.5);
+            l.setYaw(p.getLocation().getYaw());
+            p.teleport(l);
         }
     }
 }
